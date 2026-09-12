bf5952e4d2fe9444034625e2dcea33fbe2830ad3
Ali627miya <182002445+Ali627miya@users.noreply.github.com>
2026-09-12 22:10:47 +0100

    Implement Kafka transaction listener

---
+++ src/main/java/com/jpmc/midascore/TransactionListener.java
@@ -0,0 +1,19 @@
+package com.jpmc.midascore;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import com.jpmc.midascore.foundation.Transaction;
+
+@Component
+public class TransactionListener {
+
+    @KafkaListener(
+        topics = "${general.kafka-topic}",
+        groupId = "midas-core",
+        properties = "auto.offset.reset:earliest"
+    )
+    public void receive(Transaction transaction) {
+        System.out.println("RECEIVED: " + transaction);
+    }
+}
